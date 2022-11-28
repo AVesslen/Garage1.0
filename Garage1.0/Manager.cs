e704ba5bc2329a369fa3cd71d20fbc4a14b5293c
Anna Vesslén <anna_ananaz@hotmail.com>
2022-11-29 00:16:03 +0100

    Added Run() och Initialize()

--- Garage1.0/Manager.cs
+++ Garage1.0/Manager.cs
@@ -20,22 +20,15 @@ namespace Garage1._0
             this.ui = ui;
             this.handler = handler;
         }
-       
+
+        internal void Run()
+        {
+            Initialize();
+            Start();
+        }       
       
         public void Start()
-        {
-            WelcomeUser();
-           
-            //garagenamn?
-            int capacity = ui.GetIntInput("How many parking spaces do you need in your garage? ");
-            handler.CreateGarage(capacity);
-           
-            string answer=ui.GetStringInput("Do you want the garage to be filled with some vehicles from start? (yes/no)");
-            if (answer.ToLower()=="yes")
-            handler.SeedData();
-
-            
-            
+        {           
             bool isRunning = true;
             do
             {               
@@ -50,25 +43,38 @@ namespace Garage1._0
                         UnparkVehicle();
                         break;
                     case "3":
-                        ListAll();
+                        ListVehicles();
                         break;
-                    
+                    case "4":
+                        ListVehicleTypesAndCount();
+                        break;
+                    case "5":
+                        FindByRegNo();
+                        break;
+                    case "6":
+                        FindByProperties();
+                        break;
                     case "Q":
                        isRunning = false;
                         break;
                     default:
-                        ui.Print("Please enter some valid input (1, 2, 3 or Q");
+                        ui.Print("Please enter some valid input (1, 2, 3, 4, 5, 6 or Q");
                         break;
                 }
             } while (isRunning==true);
         }
 
-
-        private void WelcomeUser()
+        private void Initialize()
         {
             ui.Print("Welcome to this garage application. Let's create a garage!");
+            int capacity = ui.GetIntInput("How many parking spaces do you need in your garage? ");
+            handler.CreateGarage(capacity);
+
+            string answer = ui.GetStringInput("Do you want the garage to be filled with some vehicles from start? (yes/no)");
+            if (answer.ToLower() == "yes")
+                handler.SeedData();
         }
-       
+              
         private string ShowMainMenu()
         {
            return ("Please select what you want to do with your garage"
@@ -80,7 +86,6 @@ namespace Garage1._0
             + "\n6. Find vehicle by one or more properties"
             + "\nQ. Quit the application");
         }
-
 
         private void ParkVehicle()
         {
@@ -123,8 +128,7 @@ namespace Garage1._0
                     else isExisting = false;
                     
                 } while (isExisting == true) ;
-               
-                               
+                                             
 
                 switch (vehicleType)            // Asks for properties in sub classes
                 {
@@ -188,30 +192,30 @@ namespace Garage1._0
                 ui.Print("Your vehicle was succesfully unparked!");
         }
 
-
-
-        private void SeeStatistics()
+        private void ListVehicles()                      // Lists all vehicles in the garage
         {
-            // //1. List all vehicles in the garage
+            //ToDo: Lägg till kapacitet och antal fordon?
+            ui.Print("\nHere are the vehicles you have parked in the garage right now: \n");
+            string vehiclesInGarage = handler.ListAllVehiclesInGarage();
+            ui.Print(vehiclesInGarage);
+        }                   
 
-            // //ToDo: Lägg till kapacitet och antal fordon?
-            // ui.Print("\nHere are the vehicles you have parked in the garage right now: \n");           
-            // string vehiclesInGarage=handler.ListAllVehiclesInGarage();
-            // ui.Print(vehiclesInGarage);
+        private void ListVehicleTypesAndCount()          // Lists vehicle types and how many of them
+        {            
+            ui.Print("Number of each vehicle:\n");
+            string typeAndNumber = handler.GetNoOfEachType();
+            ui.Print(typeAndNumber);
+        }
 
+        private void FindByRegNo()                       // Finds a vehicle by registration number   
+        {
+            string inputRegNo = ui.GetStringInput("Enter the registration number of the vehicle you want to find: ");
+            string vehicleFound = handler.FindVehicleByRegNo(inputRegNo);
+            ui.Print(vehicleFound);
+        }
 
-            // //2. List vehicle types and how many of them
-            // ui.Print("Number of each vehicle:\n");
-            //string typeAndNumber=handler.GetNoOfEachType();
-            // ui.Print(typeAndNumber);
-
-            //3. Find a vehicle by registration number          
-            //string inputRegNo = ui.GetStringInput("Enter the registration number of the vehicle you want to find: ");
-            //string vehicleFound = handler.FindVehicleByRegNo(inputRegNo);          
-            //ui.Print(vehicleFound);
-
-            //4. Find vehicle by one or more properties
-            
+        private void FindByProperties()                  // Finds vehicle(s) by one or more properties
+        {
             // type
             string type = "";
             bool isRunning = false;
@@ -277,13 +281,13 @@ namespace Garage1._0
                 }
                 else
                 {
-                    answer=ui.GetStringInput("That was not a valid input. Please enter yes/no.");
+                    answer = ui.GetStringInput("That was not a valid input. Please enter yes/no.");
                     isRunning = true;
                 }
             } while (isRunning == true);
 
             // no of wheels
-            int noOfWheels = -1; 
+            int noOfWheels = -1;
             answer = ui.GetStringInput("Do you want to find vehicle by no of wheels? (yes/no)");
             do
             {
@@ -309,77 +313,8 @@ namespace Garage1._0
                 }
             } while (isRunning == true);
 
-
-           string foundVehicles = handler.FindByProperty(type, color, noOfWheels);
-           ui.Print($"This is the result of your search criteria:\n{foundVehicles}");
-
-
-
-
-
-
-            //var filteredPeople = people.Where(person => person.SecQuestions[key] == value);
-
-
-            //string amountAirplane = handler.GetNoOfEachType();
-            // ui.Print(amountAirplane);
-
-
-
-
-            // handler.SortByProperty("grey");
-
-            //string types=handler.ListType();
-            //ui.Print(types);
-
-            //What du you want to do?
-            //     1. List all parked vehicles
-            //     2. List vehicles by property
-            //    3. Find a specific vehicle by regNO
-
-            //if 3.
-            //Do you want to filter by type?
-            //  if yes
-            //    string type = ui.GetStringInput("which type? ");
-            // Do you want to filter by Color?
-            // if yes
-            //string color = ui.GetStringInput("which color? ");
-            // by rising number Of Wheels?
-            // if yes
-            //var q = handler.SortByColor("grey");
-            //q = handler.SortByNoOfWheels();
-            //foreach (var item in q)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-
-            //bool isFull=handler.CheckIfEmptyGarage();
-
-            //Garage<Vehicle> garage=handler.ListAllVehiclesInGarage();
-            //foreach (Vehicle item in handler.garage)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //var q = garage.Where(v => v?.Color == color);
-            //q = q.OrderBy(v => v.NoOfWheels);
-
-            //return q;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            string foundVehicles = handler.FindByProperty(type, color, noOfWheels);
+            ui.Print($"This is the result of your search criteria:\n{foundVehicles}");
+        } 
     }
 }
